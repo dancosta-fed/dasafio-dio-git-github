e2b3685774c9ebcd027db05a5c809b5620d43f4c
Daniel de Oliveira Costa <dancosta.id@gmail.com>
2021-12-30 21:27:20 -0300

    Contador finalizado com muitas funções legais

--- javascript/contador/assets/js/scripts.js
+++ javascript/contador/assets/js/scripts.js
@@ -1,32 +1,40 @@
-let currentNumber = document.getElementById('currentNumber');
+const currentNumber = document.getElementById("currentNumber");
+const dec = document.getElementById("decrement"); 
+const inc = document.getElementById("increment")
+
 let count = 0;
 
+function increment() {
+  dec.removeAttribute("disabled");
+  count = ++count;
+  currentNumber.innerHTML = count;
 
-    function increment(){
-        count++;
-        currentNumber.innerHTML = count;
-    }
+  if (count >= 5){
+    currentNumber.style.color = "#0494ef";
+  }
 
-       
-    function decrement(){
-        
-        count = count--;
-        currentNumber.innerHTML = count;
-        
-        
-        if(count < 0){
-            let dec = document.getElementById('decrement');
-            dec.setAttribute('disabled', 'disabled');
-            return
-        } 
-        
-    }
-    
-    // let add = document.getElementById('increment').addEventListener('click', increment());
-    // let less =document.getElementById('decrement').addEventListener('click', decrement());
-    
+  if (count >= 10) {
+    inc.setAttribute("disabled", true);
+    alert("Eita! Não dá pra ir mais que isso!");
+  } 
 
-  
+}
+
+function decrement() {
+  inc.removeAttribute("disabled");
+  count = --count;
+  currentNumber.innerHTML = count;
+
+  if (count < 5){
+    currentNumber.style.color = "#d91e2b";
+  }
+
+  if (count <= 0) {
+    dec.setAttribute("disabled", true);
+    alert("Eita! Não dá pra ir mais que isso!");
+  }
+}
+
 
 
 
